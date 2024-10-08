f89a32a070e809aa13f98d844607417d95f8394b
OlliAlaluukas <olli.alaluukas@gmail.com>
2024-10-08 17:34:17 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((data) => {
+      console.log(data);
+      setPost(data?.data);
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Firebase</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      Users
+      {post.map((item, i) => {
+        return (
+          <div key={i}>
+            <p>{item?.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
